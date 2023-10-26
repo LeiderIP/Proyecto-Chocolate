bd1617c0fbf75bcd0dd10b8c2f056a1362a54f16
LeiderIP <leiderpaez13@gmail.com>
2023-10-26 17:21:13 -0500

    pop ups and modals updated

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 (() => {
-    const refs = {
-        openModalBtn: document.querySelector("[data-modal-open]"),
-        closeModalBtn: document.querySelector("[data-modal-close]"),
-        modal: document.querySelector("[data-modal]"),
-    };
-    
-        refs.openModalBtn.addEventListener("click", toggleModal);
-        refs.closeModalBtn.addEventListener("click", toggleModal);
-    
-        function toggleModal() {
-            refs.modal.classList.toggle("is_hidden");
-        }
-    })();
+  const refs = {
+      openModalBtn: document.querySelector("[data-modal-open-0]"),
+      closeModalBtn: document.querySelector("[data-modal-close-0]"),
+      modal: document.querySelector("[data-modal-0]"),
+  };
+  
+      refs.openModalBtn.addEventListener("click", toggleModal);
+      refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+      function toggleModal() {
+          refs.modal.classList.toggle("is_hidden");
+      }
+  })();
--- src/topsellers.js
+++ src/topsellers.js
@@ -1,14 +1,14 @@
 (() => {
-    const refs = {
-        openModalBtn: document.querySelector("[data-modal-open]"),
-        closeModalBtn: document.querySelector("[data-modal-close]"),
-        modal: document.querySelector("[data-modal]"),
-    };
-    
-        refs.openModalBtn.addEventListener("click", toggleModal);
-        refs.closeModalBtn.addEventListener("click", toggleModal);
-    
-        function toggleModal() {
-            refs.modal.classList.toggle("is_hidden");
-        }
-    })();
+const refs = {
+    openModalBtn: document.querySelector("[data-modal-open-3]"),
+    closeModalBtn: document.querySelector("[data-modal-close-3]"),
+    modal: document.querySelector("[data-modal-3]"),
+};
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+    }
+})();
